593fda1dc0780c287d7764d7288ad73cf8954cdb
Sahil Bisht <sahilbisht19334@gmail.com>
2023-01-30 20:43:19 +0530

    deployed server on github

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import Connection from "./database/db.js";
+import Routes from "./routes/route.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 8000;
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/", Routes);
+
+Connection();
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
